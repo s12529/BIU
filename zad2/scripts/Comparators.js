2370fa865cfa352cfc21dfb6f5a535452d23373d
Santroy <s12529@pjwstk.edu.pl>
2016-11-22 16:08:11 +0100

    reszta sortowania

--- zad2/scripts/Comparators.js
+++ zad2/scripts/Comparators.js
@@ -3,37 +3,51 @@ function Comparators() {
 	self.state = false;
 
 	self.byName = function(person1,person2) {
-		if(self.state) {
-		return -person1.firstName.localeCompare(person2.firstName); }
+		if(self.state)
+		return -person1.firstName.localeCompare(person2.firstName);
 		else return person1.firstName.localeCompare(person2.firstName);
 	}
 
 	self.bySurname = function(person1,person2) {
+		if(self.state)
 		return person1.lastName.localeCompare(person2.lastName);
+		else return -person1.lastName.localeCompare(person2.lastName);
 	}
 
 	self.byAge = function(person1,person2) {
+		if(self.state)
 		return person1.age - person2.age;
+		else return -(person1.age - person2.age);
 	}
 
 	self.byId = function(person1,person2) {
+		if(self.state)
 		return person1.id - person2.id;
+		else return -(person1.id - person2.id);
 	}
 
 	self.byGender = function(person1,person2) {
+		if(self.state)
 		return person1.gender.localeCompare(person2.gender);
+		else return -person1.gender.localeCompare(person2.gender);
 	}
 
 	self.byEmail = function(person1,person2) {
+		if(self.state)
 		return person1.email.localeCompare(person2.email);
+		else return -person1.email.localeCompare(person2.email);
 	}
 
 	self.byBirthsday = function(person1,person2) {
+		if(self.state)
 		return person1.birthsday.localeCompare(person2.birthsday);
+		else return -person1.birthsday.localeCompare(person2.birthsday);
 	}
 
 	self.byIncome = function(person1,person2) {
+		if(self.state)
 		return person1.income - person2.income;
+		else return -(person1.income - person2.income);
 	}
 
 	self.setState = function() {
